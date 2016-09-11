820c9eb9383cb14f7f049b2d1531ea1e9e11f77b
Noah Edward Hall <noahedwardhall@gmail.com>
2016-09-11 16:23:55 -0700

    add testing framework

---
+++ src/containers/page/page.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import Page from './page.js';
+
+describe("FILE: src/containers/page.js", () => {
+  it("is okay", () => {
+    expect(Page).to.be.ok;
+  });
+});
